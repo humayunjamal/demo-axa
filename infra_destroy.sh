1464cdd33c2c0923cfbbac690cc3cba5599a300c
Humayun Jamal <humayunjamal@hotmail.com>
2018-12-06 15:02:45 +0400

    final fix

--- infra_destroy.sh
+++ infra_destroy.sh
@@ -4,25 +4,25 @@
 cd terraform/staging/service/ec2-jenkins
 
 terraform init
-terraform apply -auto-approve
+terraform destroy -auto-approve
 
 cd ../ecs-demo-service
 
 terraform init
-terraform apply -auto-approve
+terraform destroy -auto-approve
 
 
 cd ../ecs-demo-service-uat
 
 terraform init
-terraform apply -auto-approve
+terraform destroy -auto-approve
 
 cd ../ecs-infra
 
 terraform init
-terraform apply -auto-approve
+terraform destroy -auto-approve
 
 cd ../../
 
 terraform init
-terraform apply -auto-approve
+terraform destroy -auto-approve
