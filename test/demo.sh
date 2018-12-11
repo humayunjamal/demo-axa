1275851f785886ca9c4ae923ca0b535da07fc317
Humayun Jamal <humayunjamal@hotmail.com>
2018-12-11 21:59:12 +0400

    test pass

--- test/demo.sh
+++ test/demo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 Content=$(curl http://demo-service-uat.demo-axa.co.uk)
-ContentB="HELLO THIS IS THE DEMO version 1.1"
+ContentB="HELLO THIS IS THE DEMO version 1.1 "
 
 if [ "$Content" = "$ContentB" ]; then
     echo "Content Matched"
