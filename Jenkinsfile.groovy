eb119d1c4fb42f0f7f7a36379465521da48b8dcd
Muhammad Husain <muhammad.husain@hmcts.net>
2018-12-11 17:39:54 +0000

    Added test to demo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,72 +1,74 @@
 pipeline {
-  agent any
-  options {
-    disableConcurrentBuilds()
-  }
-  stages {
-    stage('CREATE TEMP ENV') {
-      steps {
-        sh "echo Creating local ENVIRONMENT for testing"
-        sh "sleep 5"
-
-  }
+    agent any
+    options {
+        disableConcurrentBuilds()
     }
-    stage('RUN  UNIT TESTS') {
+    stages {
+        stage('CREATE TEMP ENV') {
+            steps {
+                sh "echo Creating local ENVIRONMENT for testing"
+                sh "sleep 5"
 
-      steps {
-        sh "echo RUNNING UNIT TESTS against local TEMP ENVIRONMENT"
-        sh "sleep 5"
+            }
+        }
+        stage('RUN  UNIT TESTS') {
 
-      }
-      
-    }
-    
+            steps {
+                sh "echo RUNNING UNIT TESTS against local TEMP ENVIRONMENT"
+                sh "sleep 5"
 
-   
-    stage('CREATE UAT ENVIRONMENT') {    
-      steps {
-        echo "Running Terraform to create UAT ENVIRONMENT"
-        sh """
+
+            }
+
+        }
+
+
+
+        stage('CREATE UAT ENVIRONMENT') {
+            steps {
+                echo "Running Terraform to create UAT ENVIRONMENT"
+                sh """
         pwd
         ./deploy_uat.sh
         cd terraform/staging/service/ecs-demo-service-uat
         /usr/bin/terraform init
         terraform plan
         """
-        
-      }
-    }
 
-    stage('DEPLOY TO UAT ENVIRONMENT') {    
-      steps {
-        echo "Starting deployment on UAT ENVIRONMENT"
-        sh """
+            }
+        }
+
+        stage('DEPLOY TO UAT ENVIRONMENT') {
+            steps {
+                echo "Starting deployment on UAT ENVIRONMENT"
+                sh """
         ecs-deploy -c Demo-Cluster -n demo-service-uat -to latest -i demo-service-uat --region eu-west-1 
         """
 
-      }
-    }
+            }
+        }
 
-    stage('TESTING UAT ENVIRONMENT') {    
-      steps {
-        echo "Starting UAT ENVIRONMENT TESTS"
-        sh "sleep 5"
+        stage('TESTING UAT ENVIRONMENT') {
+            steps {
+//                echo "Starting UAT ENVIRONMENT TESTS"
+//                sh "sleep 5"
+                sh "./test/demo.sh"
 
-      }
-    }
+            }
+        }
 
-     stage('DEPLOY TO PROD') {
-   
-      steps {
-        echo "Starting deployment on Prod"
-        sh """
+        stage('DEPLOY TO PROD') {
+
+            steps {
+                echo "Starting deployment on Prod"
+                sh """
         pwd
         ls -ltrh
         ./deploy.sh
         """
-      }
+            }
+        }
+
     }
-    
-  }
 }
 
---
+++ test/demo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+Content=$(curl http://demo-service-uat.demo-axa.co.uk)
+ContentB="HELLO THIS IS THE DEMO version 1.1"
+
+if [ "$Content" = "$ContentB" ]; then
+    echo "Content Matched"
+else
+    echo "Content is NOT Matched"
+	exit 64
+fi;
